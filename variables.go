92eaf1c351e24cb9b0a17ba72e680109d0f9e18f
Gideon Opoku <opokugideon562@gmail.com>
2022-12-09 22:55:45 +0000

    Create variables.go

---
+++ variables.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	//	var (
+	//		name string = "User"
+	//		age int = 45
+	//		location string = "Ghana"
+	//	)
+	//fmt.Println(name)
+	//fmt.Println(location)
+	//fmt.Println(age)
+	//var i int
+	//i = 3
+	//var j int = 23
+	//k := 45
+	//fmt.Println(i)
+
+	var i int = 48
+	fmt.Printf("%v, %T\n", i, i)
+
+	var j int = 23
+	var k float32
+	fmt.Printf("%v, %T\n", k, k)
+	k = float32(j)
+
+	var s string
+	s = strconv.Itoa(i)
+	fmt.Printf("%v, %T\n", s, s)
+}
