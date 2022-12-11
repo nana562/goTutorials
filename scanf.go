0d6e3bdc725944318b4fff77e1205b1570a499a2
Gideon Opoku <opokugideon562@gmail.com>
2022-12-11 03:16:32 +0000

    Create scanf.go

---
+++ scanf.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+y:=
+func main(){
+	//fmt.Println("Hello Gideon")
+	/*
+	comments
+	
+	fmt.Println("Hello Gideon")
+	fmt.Println("2 + 3 = ", 2 + 3)
+	fmt.Println("2 - 3 = ", 2.5 - 3.0)
+	fmt.Println("2 * 3 = ", 2 * 3)
+	fmt.Println("2 / 3 = ", 2 / 3)
+	fmt.Println("2 % 3 = ", 2 % 3)
+
+	fmt.Println(len("Hello")) //length of the string
+	fmt.Println("hello World" [0]) //shows the byte of the index element
+	fmt.Println("Welcome" + " " + "Gideon")
+
+	//Variables
+	//var msg string = "Welcome to Go lang"
+	//fmt.Println(msg)
+
+	
+
+	fmt.Println("Enter a number: ")
+	var x int
+	fmt.Scanf("%d", &x)
+	output := x * 2
+	fmt.Println(output)
+
+	// c = (f - 32) * 5/9
+	
+	//Farhenheit to Celsius Converter
+	fmt.Println("Enter a temperature in Fahrenheit: ")
+	var f float64
+	fmt.Scanf("%f", &f)
+	c:=(f-32*5/9)
+	fmt.Println(c)
+*/
+	fmt.Println("Enter a value for feet")
+	var f float64
+	fmt.Scanf("%f", &f)
+	m := f * 0.3048
+	fmt.Println(m + 'm')
+	fmt.Println(y)
+
+	func number(){
+		fmt.Println(y)
+	}
+}
